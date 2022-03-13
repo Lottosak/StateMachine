20a45b3caa1037735562e962590097390c7af3fa
lukas nemec <lukasnemec@lukass-MacBook-Air.local>
2022-03-13 19:14:47 +0100

    refactoring

--- StateMachine.hpp
+++ StateMachine.hpp
@@ -7,38 +7,48 @@ template <typename... States>
 class StateMachine
 {
 private:
+    // tuple of machine states
     std::tuple<States...> states;
-    std::variant<States*...> currentState(&std::get<0>(states));
+    // current state
+    std::variant<States *...> currentState(&std::get<0>(states));
+
 public:
     StateMachine();
     ~StateMachine();
 
+    // change currentState to state
     template <typename State>
     void transitionTo()
     {
         currentState = &std::get<State>(states);
     }
 
+    // handle event, callback execute on current state
     template <typename Event>
-    void handle(const Event& event)
+    void handle(const Event &event)
     {
-        std::visit([&event] (auto statePtr) { statePtr->handle(event).execute(*this); }, currentState);
+        std::visit([&event](auto statePtr)
+                   { statePtr->handle(event).execute(*this); },
+                   currentState);
     }
 };
 
-template<typename State>
+// transition struct -> executes machine transition
+template <typename State>
 struct TransitionTo
 {
     template <typename Machine>
-    void execute(Machine &machine){
-        machine.template transitionTo<State>();
+    void execute(Machine &machine)
+    {
+        machine.transitionTo<State>();
     }
 };
 
+// donothing struct
 struct DoNothing
 {
     template <typename Machine>
-    void execute(Machine &machine){
-
+    void execute(Machine &machine)
+    {
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,91 +1,59 @@
-//#include <iostream>
-//#include "StateMachine.hpp"
 #include <iostream>
-#include <tuple>
-#include <variant>
-#include <functional>
+#include "StateMachine.hpp"
 
-template <typename... States>
-class StateMachine
-{
-private:
-    std::tuple<States...> states;
-    std::variant<States*...> currentState(&std::get<0>(states));
-public:
-    StateMachine();
-    ~StateMachine();
-
-    template <typename State>
-    void transitionTo()
-    {
-        currentState = &std::get<State>(states);
-    }
-
-    template <typename Event>
-    void handle(const Event& event)
-    {
-        std::visit([&event] (auto statePtr) { statePtr->handle(event).execute(*this); }, currentState);
-    }
-};
-
-template<typename State>
-struct TransitionTo
-{
-    template <typename Machine>
-    void execute(Machine &machine){
-        machine.template transitionTo<State>();
-    }
-};
-
-struct DoNothing
-{
-    template <typename Machine>
-    void execute(Machine &machine){
-
-    }
-};
-//test example - patroling
-//decision tree - walkA -> lookAround -> walkB
+// test example - patroling
+// use case - walk -> arrive -> patrol -> stop patroling -> walk
 struct WalkState;
-struct LookingState;
+struct PatrolingState;
 
-struct ArriveEvent {};
-struct FinishLookingEvent {};
+struct ArriveEvent
+{
+};
+struct FinishPatrolingEvent
+{
+};
 
-struct WalkState{
-    TransitionTo<LookingState> handle(const ArriveEvent&)
+// Walking state -> when arrive event transition to patroling otherwise do nothing
+struct WalkState
+{
+    TransitionTo<PatrolingState> handle(const ArriveEvent &)
     {
         std::cout << "Arrived at destination." << std::endl;
         return {};
     }
 
-    DoNothing handle(const FinishLookingEvent&){
+    DoNothing handle(const FinishPatrolingEvent &)
+    {
         std::cout << "Already finished looking (in walking state)." << std::endl;
         return {};
     }
 };
 
-struct LookingState{
-    TransitionTo<WalkState> handle(const FinishLookingEvent&)
+// Patroling state -> when finished patroling then start walking else do nothing
+struct PatrolingState
+{
+    TransitionTo<WalkState> handle(const FinishPatrolingEvent &)
     {
         std::cout << "Finished looking around." << std::endl;
         return {};
     }
 
-    DoNothing handle(const ArriveEvent&){
+    DoNothing handle(const ArriveEvent &)
+    {
         std::cout << "Already in looking state." << std::endl;
         return {};
     }
 };
 
-using PatrolingSM = StateMachine<WalkState,LookingState>;
+using PatrolingSM = StateMachine<WalkState, PatrolingState>;
 
-int main(void){
+int main(void)
+{
 
-    //start walking to point
+    // start walking to point
     auto patrolingSM = PatrolingSM();
-    //patroling
+    // patroling
     patrolingSM.handle(ArriveEvent());
     // start walking to another point
-    patrolingSM.handle(FinishLookingEvent());
+    patrolingSM.handle(FinishPatrolingEvent());
 }
